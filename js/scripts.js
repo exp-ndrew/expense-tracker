10b595bfc609f881bb50673746e7ab1de2f45fa4
Andrew M Westling & Patrick David <student@epicodus.com>
2014-07-18 15:52:55 -0700

    Specs Pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,89 @@
  var NewCategory = {
-  theme: "",
   items: [],
-  totalCost: function(price){
-    for (var i=0; i<price.length; i++) {
-      this.total = this.total + price[i];
-    }
-  }
+  // totalCost: function(price){
+  //   var grandTotal = 0;
+  //   for (var i=0; i<price.length; i++) {
+  //     this.total = this[[items[i]].price] + price[i];
+  //   }
+  // }
 };
 
- var Purchase = {
+var Purchase = {
   description: "",
   price: 0,
+  quantity: 0,
+  total: function(){
+    return this.price * this.quantity;
+  }
 };
 
 $(document).ready(function() {
 
-  var currentList = "";
+  var currentList;
 
+//CLICK THE CATEGORY ADD BUTTON
   $("#category-add").click(function(event) {
 
     event.preventDefault();
     var catName = $("input#new-category").val();
-    $("#category-list").append("<li class="category-list-item">" + catName + "</li>");
+
+    //appending to the lists list
+    $("#category-list").append("<li class='category-list-item'>" + catName + "</li>");
     $(".selected-category").text(catName);
+
+    //set the current list to this input
     currentList = catName;
-    console.log(currentList)
-    newCategory = Object.create(NewCategory); // creates the object
-    console.log(newCategory);
+
+    //build the category object
+    category = Object.create(NewCategory); // creates the object
+    category.name = catName;
+
     $("input#new-category").val("");
+
+
+
+//CLICK A LIST ITEM
+    $(".category-list-item").last().click(function(event) {
+      currentList = catName;
+      $(".selected-category").text(catName);
+      $("#purchase-table").empty();
+      currentList.items.forEach(function(item) {
+        $("#purchase-table").append("<tr><td>" + currentList[items].description + "</td>" + "<td>$" + currentList[items].price + "</td></tr>");
+      });
+      //populate the right side with the items in the object/array/whatever
+    });
   });
 
-  $(".category-list-item").last().click(function(event) {
-    currentList = $(".category-list-item").val();
-
-    console.log(currentList);
 
 
+
+//CLICK THE PURCHASE ADD BUTTON
+  $("#purchase-add").click(function(event){
+    var newDescription = $("#new-description").val();
+    var newPrice = $("#new-price").val();
+    var newQuantity = $("#new-quantity").val();
+
+    //build the purchase object
+    var purchase = Object.create(NewPurchase);
+    purchase.description = $("#new-description").val();
+    purchase.price = parseFloat($("#new-price").val());
+    purchase.quantity = $("#new-quantity").val();
+    category.items.push(purchase);
+
+    console.log(purchase.total());
+
+    //append to the purchases list
+    $("#purchase-table").append("<tr><td>" + purchase.description + "</td><td>$" + purchase.price + "</td><td>" + purchase.quantity + "</td><td>$" + purchase.total() + "</td></tr>");
+
+    //fill in the grand total box
+    $("#purchase-total").text("<td>Your total is $" + category.total() + ". </td>")
+
+    // console.log(category)
+
+    //clear the form
+    $("input#new-description").val("");
+    $("input#new-price").val("");
+    $("#new-quantity").val("");
   })
-
-
-  // $("form#new-description".submit(function(event) {
-  //   var inputtedDescription = $("input#description").val();
-  //   var inputtedAmount = $("input#amount")
-  // }
-
 
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,16 @@
 describe('Purchase', function() {
-  describe("totalCost", function() {
+  describe("total", function() {
     it("adds single amount to Purchase.total", function(){
       var testPurchase = Object.create(Purchase);
-      testPurchase.totalCost([10]);
-      testPurchase.total.should.equal(10);
+      testPurchase.quantity = 2;
+      testPurchase.price = 10;
+      testPurchase.total().should.equal(20);
     });
     it("adds multiple amounts together in Purchase.total", function(){
       var testPurchase = Object.create(Purchase);
-      testPurchase.totalCost([10, 12, 13.50]);
-      testPurchase.total.should.equal(35.50);
+      testPurchase.quantity = 1;
+      testPurchase.price = 10;
+      testPurchase.total().should.equal(10);
     });
   });
 });
